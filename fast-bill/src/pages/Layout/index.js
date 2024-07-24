88c13978d50af3dd5d05ee749d4257508c0a3ac0
chen <alf138540fun@gmail.com>
2024-07-24 23:01:21 +0200

    use theme color

--- fast-bill/src/pages/Layout/index.js
+++ fast-bill/src/pages/Layout/index.js
@@ -1,9 +1,11 @@
+import { Button } from "antd-mobile"
 import { Outlet } from "react-router"
 
 const Layout = () => {
     return <div>
     <Outlet/>
     Layout
+    <Button color="primary">test global</Button>
     </div>
 }
 
