5bb8ea760b639a0433ec74c4f0ae426f4f3068fd
chen <alf138540fun@gmail.com>
2024-07-25 12:46:03 +0200

    use antD Tabbar realize static place

--- fast-bill/src/pages/Layout/index.js
+++ fast-bill/src/pages/Layout/index.js
@@ -1,9 +1,32 @@
-import { Button } from "antd-mobile"
 import { Outlet } from "react-router"
 import { useEffect } from "react"
 import { useDispatch } from "react-redux"
 import { getBillList } from "@/store/modules/billStore"
-
+import { TabBar } from "antd-mobile"
+import { useNavigate } from "react-router"
+import './index.scss'
+import {
+    BillOutline,
+    CalculatorOutline,
+    AddCircleOutline,
+} from 'antd-mobile-icons'
+  const tabs = [
+    {
+      key: '/month',
+      title: 'Month Bill',
+      icon: <BillOutline />,
+    },
+    {
+      key: '/new',
+      title: 'New Bill',
+      icon: <AddCircleOutline />,
+    },
+    {
+      key: '/year',
+      title: 'Yearly Bill',
+      icon: <CalculatorOutline />
+    },
+  ]
 
 const Layout = () => {
     const dispatch = useDispatch()
@@ -11,11 +34,31 @@ const Layout = () => {
         dispatch(getBillList())
     },[dispatch])
 
-    return <div>
-    <Outlet/>
-    Layout
-    <Button color="primary">test global</Button>
+    const navigate = useNavigate()
+    const switchRoute = (path) =>{
+        console.log(path)
+        navigate(path)
+    }
+
+    return (
+        <div className="Layout">
+      <div className="container">
+        container
+        {/* 二级路由出口 */}
+        <Outlet />
+      </div>
+      
+      <div className="footer">
+      <TabBar
+      onChange={switchRoute}
+      >
+        {tabs.map(item => (
+            <TabBar.Item key={item.key} icon={item.icon} title={item.title} />
+        ))}
+      </TabBar>
+        </div>
     </div>
+    )
 }
 
 export default Layout
